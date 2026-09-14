da8ebeb827d2b7469962613927f5a7b868e603d4
khasim938012 <khasim938012@gmail.com>
2026-09-14 22:56:33 +0530

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const mongoose = require('mongoose');
+const path = require('path');
+
+
+app.set("views",path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+main().then(() => {
+    console.log("Connected to MongoDB")
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+app.get("/", (req, res) => {
+    res.send(" root is working");
+});
+
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
